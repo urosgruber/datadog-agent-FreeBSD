2cd2aac14e6df3bda49c2822c67d18190f457bf7
Uros Gruber <uros@gruber.si>
2020-10-24 20:33:48 +0200

    final

--- files/patch-rtloader_common_rtloader__mem.h
+++ files/patch-rtloader_common_rtloader__mem.h
@@ -1,6 +1,6 @@
---- rtloader/common/rtloader_mem.h.orig	2020-01-31 16:08:09.641074000 +0000
-+++ rtloader/common/rtloader_mem.h	2020-01-31 15:53:55.781730000 +0000
-@@ -69,6 +69,8 @@
+--- rtloader/common/rtloader_mem.h.orig	2020-10-24 17:12:10 UTC
++++ rtloader/common/rtloader_mem.h
+@@ -69,6 +69,8 @@ void _free(void *ptr);
  #        define __THROW
  #    elif __APPLE__
  #        define __THROW
